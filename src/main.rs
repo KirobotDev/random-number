4351b4dc30b202a1734485da1fb921338cf22529
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-06-19 01:58:56 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,25 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    let mut i = 0
-    while i > 3{
-        let mut s = String::new();
+    let mut s = String::new();
     
-        let number = rand::thread_rng().gen_range(1..101);
+    let number = rand::thread_rng().gen_range(1..101);
     
-        io::stdin()
-            .read_line(&mut s)
-            .expect("Error");
+    io::stdin()
+        .read_line(&mut s)
+        .expect("Error");
     
-        let nb: i32 = s.trim().parse().expect("Error");
+    let nb: i32 = s.trim().parse().expect("Error");
     
-        if nb == number {
-            println!("Tu a gagné")
-        }
+    if nb == number {
+        println!("Tu a gagné")
+    }
     
-        else if nb != number {
-            println!("le nombre et bien différents")
-        }
+    else if nb != number {
+        println!("le nombre et bien différents")
+    }
     
-        else {
-            println!("Tu a perdu")
-        }
+    else {
+        println!("Tu a perdu")
     }
 }   
